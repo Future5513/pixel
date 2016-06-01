ad0d15da5f6996a7402a7fe955f99b424b444b19
Future5513 <future@futuredemacbook-air.local>
2016-06-01 10:34:37 +0800

    Initial Commit

---
+++ pixel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pixel
+//
+//  Created by future on 16/6/1.
+//  Copyright © 2016年 future. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
